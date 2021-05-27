9a4f71b0555fab82ba18d3383e86c72131c1e0d2
Fernando de Jesús <81062875+fermendivilt@users.noreply.github.com>
2021-05-27 08:54:21 -0700

    Headers con puntero con vector

--- Actividad6_Polimorfismo/main.cpp
+++ Actividad6_Polimorfismo/main.cpp
@@ -2,23 +2,31 @@
 //TC1030 Grupo 700
 //24 de mayo del 2021
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "Rectangulo.h"
 #include "Circulo.h"
 
 int main(){
-    Forma *Formas[5]; // arreglo de objetos Forma
+    // Forma *Formas[5]; // arreglo de objetos Forma
+    vector<Forma*> vecFormas;
+    Forma *objFormas;
 
-    Formas[0] = new Circulo();
-    Formas[1] = new Rectangulo();
-    Formas[2] = new Rectangulo(2, 3);
-    Formas[3] = new Circulo(0, 0, 4);
-    Formas[4] = new Rectangulo();
+    objFormas = new Circulo(5,6,7);
+    vecFormas.push_back(objFormas);
 
-    for (int i = 0; i<5; i++){
-        Forma *actual = Formas[i];
-        actual->draw();
+    objFormas = new Rectangulo(5,8);
+    vecFormas.push_back(objFormas);
+
+    objFormas = new Circulo(3,5,9);
+    vecFormas.push_back(objFormas);
+
+    objFormas = new Rectangulo(2,3);
+    vecFormas.push_back(objFormas);
+
+    for (int i = 0; i<vecFormas.size(); i++){
+        vecFormas[i]->draw();
     }
 
     return 0;
